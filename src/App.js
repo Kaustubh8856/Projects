58925d01cd01cf92ce6f4086e299beae063f93f0
Kaustubh <kaustubh8856@gmail.com>
2024-06-06 17:58:35 +0530

    ImageSlider-Interval

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import Accordian from "./components/accordion/index.jsx";
+import ImageSlider from "./components/image-slider/index.jsx";
 import RandomColor from "./components/random-color/index.jsx";
 import StarRating from "./components/star-rating/index.jsx";
 
 function App() {
   return (
     <div className="App">
-    {/* <Accordian/> */}
-    {/* <RandomColor/> */}
-    <StarRating/>
+      {/* <Accordian/> */}
+      {/* <RandomColor/> */}
+      {/* <StarRating/> */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
