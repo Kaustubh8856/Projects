60d1c2787769d54a1f8c0eca70053aa9a432b030
Kaustubh <kaustubh8856@gmail.com>
2024-06-10 18:04:35 +0530

    loadMore - completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Accordian from "./components/accordion/index.jsx";
 import ImageSlider from "./components/image-slider/index.jsx";
+import LoadMore from "./components/loadMore/index.js";
 import RandomColor from "./components/random-color/index.jsx";
 import StarRating from "./components/star-rating/index.jsx";
 
@@ -10,11 +11,12 @@ function App() {
       {/* <Accordian/> */}
       {/* <RandomColor/> */}
       {/* <StarRating/> */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+      <LoadMore/>
     </div>
   );
 }
